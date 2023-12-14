4a893cde95793159f39be0598889bde8c8fca029
Astra-001 <indianarmy2625@gmail.com>
2023-12-14 12:37:34 +0530

    changing-status

--- cypress/e2e/Appointments_Sec.cy.js
+++ cypress/e2e/Appointments_Sec.cy.js
@@ -36,11 +36,9 @@ describe('TestSuites ', function () {
         cy.get('[value="e957e195-da63-4112-890a-26b086477ad1"]').click() //Selecting attribute from the drop down
         cy.get(':nth-child(1) > .css-0 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click() //Callender Icon
         cy.get('[aria-rowindex="6"] > .MuiButtonBase-root').click() //Select Date
-        cy.get(':nth-child(2) > .css-0 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click() //Select Date Icon
-        cy.get('[aria-label="2 hours"]').click() //Select Time
-        cy.get('.MuiDialogActions-root > .MuiButtonBase-root').click() //Ok
-        cy.get('.MuiInputBase-root > #assignedTo').click() //Sales Drop down
-        cy.get('[data-value="1dc96152-4f00-481f-9006-9febe15163f5"]').click() //Select Attribute from the drop down
+        cy.get('.MuiInputBase-root > #assignedTo').click()
+        cy.get('[data-value="40cdb141-7348-455b-96b1-f77327515c79"]').click()
+        //cy.get(':nth-child(2) > .css-0 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click() //Select Attribute from the drop down
         cy.get('.css-k3gutg > :nth-child(1)').click() // Time Duration(15 min)
         cy.get('.MuiAutocomplete-popupIndicator').click() //Contact Drop down
         cy.get('#contact-option-6').click() // Select Contact from the drop down
@@ -49,6 +47,30 @@ describe('TestSuites ', function () {
         cy.get('#sendSms > .MuiTypography-root').click() //  Communication Button (Send SMS)
         cy.get('.d-flex-3 > .MuiButton-contained').should('be.visible').should('be.enabled').click() //Submit Button
 
+    })
+
+    it('Create  Appointments_2', function(){
+      cy.visit("https://vibras.com/");  
+      cy.get('#email').type('webt580@gmail.com'); 
+      cy.get('#password').type('987654321');
+      cy.get('.MuiButton-root').click() 
+      cy.wait(10000);
+      cy.get(':nth-child(3) > .MuiButtonBase-root').click()
+      cy.wait(10000);
+      cy.get('#composition-button').click() 
+      cy.get('[value="e957e195-da63-4112-890a-26b086477ad1"]').click() //Selecting attribute from the drop down
+      cy.get(':nth-child(1) > .css-0 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click() //Callender Icon
+      cy.get('[aria-rowindex="6"] > .MuiButtonBase-root').click() //Select Date
+      cy.get(':nth-child(2) > .css-0 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click()
+      cy.get('.css-k3gutg > :nth-child(1)').click() // Time Duration(15 min)
+      cy.get('.MuiAutocomplete-popupIndicator').click() //Contact Drop down
+      cy.get('#contact-option-6').click()
+      cy.get('.MuiInputBase-root > #assignedTo').click()
+      cy.get('[data-value="40cdb141-7348-455b-96b1-f77327515c79"]').click()
+      cy.get('#appointmentDetails').click().type('Hey! I am on my duty') //Appointment Details
+      cy.get('#sendEmail > .MuiTypography-root').click() //Communication Button (Send Email)
+      cy.get('#sendSms > .MuiTypography-root').click() //  Communication Button (Send SMS)
+      cy.get('.d-flex-3 > .MuiButton-contained').click() //Submit Button
     })
 
     it('Multiple Day,s Appointments', function(){
@@ -60,7 +82,7 @@ describe('TestSuites ', function () {
       cy.get(':nth-child(3) > .MuiButtonBase-root').should('be.visible').click() //Navigate Appointments section
       cy.wait(10000);
       cy.get('#composition-button').click() //Create Appointment button
-      cy.get('[value="b7b24805-a0f3-4781-91e4-9e7de0e1bac4"]').click()//Selecting attribute from the drop down
+      cy.get('[value="b7b24805-a0f3-478aakash@webspero.com1-91e4-9e7de0e1bac4"]').click()//Selecting attribute from the drop down
       cy.get(':nth-child(2) > .MuiBox-root > .MuiFormControlLabel-root > .MuiTypography-root').click() //Radio button (Multiple day's Appointments)
       cy.get(':nth-child(1) > .css-0 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiButtonBase-root').click() //Callender Icon
       cy.get('[data-timestamp="1703788200000"]').click() //Select Start Date
@@ -96,4 +118,22 @@ describe('TestSuites ', function () {
     })
       
     
+
+    it.only('Edit  Appointments', function(){
+      cy.visit("https://vibras.com/");  
+      cy.get('#email').type('webt580@gmail.com'); 
+      cy.get('#password').type('987654321');
+      cy.get('.MuiButton-root').click() 
+      cy.wait(10000);
+      cy.get(':nth-child(3) > .MuiButtonBase-root').click()
+      cy.wait(10000);
+      cy.get(':nth-child(1) > .fc-event > .calender-event').click() // Schedule Appointment
+      cy.get('.view-details > .MuiGrid-container > .MuiGrid-grid-md-2 > .MuiBox-root > :nth-child(1)').click() //Click on Edit Action
+    })
+      
+   
+
+
+ 
+
 })
