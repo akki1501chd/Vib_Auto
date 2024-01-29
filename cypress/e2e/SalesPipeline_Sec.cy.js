a02894c512e27eb331d92543abc3fe7b4157c513
Astra-001 <indianarmy2625@gmail.com>
2024-01-29 16:43:15 +0530

    changing-status

--- cypress/e2e/SalesPipeline_Sec.cy.js
+++ cypress/e2e/SalesPipeline_Sec.cy.js
@@ -16,20 +16,20 @@ describe('TestSuites ', function () {
           cy.get('.portal-logo').should('be.visible') //Logo Validation
           cy.wait(10000);
           cy.get('.css-1ph3js4').click() //Navigate to List
-          cy.get('.page-container').should('be.visible')
+          //cy.get('.page-container').should('be.visible')
           cy.get('.css-zq4gof').click() //Navigate Back to Board
           cy.get('.btn_gp_sale_gp > :nth-child(1)').should('be.visible').should('be.enabled').click() //Click on New Lead
           cy.get('.MuiAutocomplete-popupIndicator').click() //Select Contact Drop Down
           cy.get('#contact-option-6').click()
-          cy.get('#dealName').type('dsass')
-          cy.get('#dripJob > .MuiTypography-root').click()
+          cy.get('#dealName').type('Vib_Deal')
+          //cy.get('#dripJob > .MuiTypography-root').click()
           cy.get('.MuiInputBase-root > #stage').click() //Drop down stages
           cy.get('[data-value="b28dc97e-dcf4-4183-979b-c82afb9cf61e"]').click() 
           cy.get('.d-flex-3 > .MuiButton-contained').should('be.visible').should('be.enabled').click() //Submit Button
-          cy.get('#composition-button').should('be.visible').should('be.enabled').click() //Click on Appointment
-          cy.get('#composition-menu').should('be.visible') 
-          cy.get('.btn_gp_sale_gp > :nth-child(3)').should('be.visible').should('be.enabled').click() //Proposals Button
-          cy.go('back');
+          //cy.get('#composition-button').should('be.visible').should('be.enabled').click() //Click on Appointment
+          //cy.get('#composition-menu').should('be.visible') 
+          // cy.get('.btn_gp_sale_gp > :nth-child(3)').should('be.visible').should('be.enabled').click() //Proposals Button
+          // cy.go('back');
           // cy.get(':nth-child(1) > .scroll-board > .droppable > .css-79elbk').drag(':nth-child(2) > .scroll-board > .droppable')
         
           
---
+++ cypress/e2e/fixture.cy.js
@@ -0,0 +1,21 @@
+cypress/integration/myTest.spec.js
+
+describe('My Test', () => {
+    it('should use a fixture', () => {
+      // Load the contents of the 'example.json' fixture
+      cy.fixture('example.json').then((data) => {
+        // 'data' now contains the content of the 'example.json' fixture
+        cy.log('Fixture Data:', data);
+  
+        // Use the fixture data in your test
+        cy.visit(data.url);
+        cy.get('#email').type(data.email);
+        cy.get('#password').type(data.password);
+        cy.get('.MuiButton-root').click(); 
+  
+        // Continue with your test... 
+
+      });
+    });
+ });
+  
